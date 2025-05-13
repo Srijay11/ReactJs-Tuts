e5ab46218c090d6e2773c2cb012cbf5d221fc069
Srijay <vsrijay11@gmail.com>
2025-05-13 10:54:45 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from React"
+// );
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Hello lol"),
+    React.createElement("h2", {}, "Hello lol"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello lol2"),
+    React.createElement("h2", {}, "Hello lol2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
